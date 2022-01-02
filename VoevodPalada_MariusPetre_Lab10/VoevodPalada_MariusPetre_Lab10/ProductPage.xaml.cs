8ec89db41ef3c8b85e52c5ede80c40bb70c1552a
Marius Petre Voevod Palada <Voevod.Ni.Marius@student.utcluj.ro>
2022-01-02 23:11:47 +0200

    Tema lab11

---
+++ VoevodPalada_MariusPetre_Lab10/VoevodPalada_MariusPetre_Lab10/ProductPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VoevodPalada_MariusPetre_Lab10.Models;
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
+using Xamarin.Forms.PlatformConfiguration.WindowsSpecific;
+using Xamarin.Forms.Xaml;
+
+namespace VoevodPalada_MariusPetre_Lab10
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ProductPage : ContentPage
+    {
+        ShopList sl;
+        public ActivityPage(ShopList slist)
+        {
+            InitializeComponent();
+            sl = slist;
+        }
+       
+        async void OnSaveButtonClicked(object sender, EventArgs e)
+        {
+            var product = (Product)BindingContext;
+            await App.Database.SaveProductAsync(product);
+            ListView.ItemsSource = await App.Database.GetProductsAsync();
+        }
+        async void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+            var product = (Product)BindingContext;
+            await App.Database.DeleteProductAsync(product);
+            ListView.ItemsSource = await App.Database.GetProductsAsync();
+        }
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            ListView.ItemsSource = await App.Database.GetProductsAsync();
+        }
+        async void OnListViewItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+
+            Product p;
+            if (e.SelectedItem != null)
+            {
+                p = e.SelectedItem as Product;
+                var lp = new ListProduct()
+                {
+                    ShopListID = sl.ID,
+                    ProductID = p.ID
+                };
+                await App.Database.SaveListProductAsync(lp);
+                p.ListProducts = new List<ListProduct> { lp };
+
+                await Navigation.PopAsync();
+            }
+        }
+}
